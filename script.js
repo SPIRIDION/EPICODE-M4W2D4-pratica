ebe7d967ab4cfdb66ed52ccda8c10a1a5038ef66
SPIRIDION <ca_messy@hotmail.it>
2025-02-17 18:55:48 +0100

    second commit

--- script.js
+++ script.js
@@ -10,13 +10,29 @@ const baseUrl = 'https://striveschool-api.herokuapp.com/books'
 
 function getBooks() {
   fetch (baseUrl)
-    .then(response => response.json)
+    .then(response => response.json())
     .then((data) => {
       console.log(data)
+      renderBooks(data)
     })
     .catch(error => console.log(error))
 } 
 
+getBooks()
+
 // creiamo la funzione che renderizzerà le card con i vari libri al suo interno
 
-// function renderBooks()
+function renderBooks(data) {
+  for (book of data) {
+    const cardBook = document.createElement('div')
+    cardBook.classList.add('col-6','col-md-4','col-lg-3')
+    const cardInside = document.createElement('div')
+    cardInside.classList.add('card')
+    cardBook.appendChild(cardInside)
+    const cardImg = document.createElement('img')
+    cardImg.classList.add('card-img-top')
+    cardImg.setAttribute('src',book.img)
+    cardInside.appendChild(cardImg)
+    mainSection.appendChild(cardBook)
+  }
+}
